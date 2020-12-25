f4844cec697fb844dbfac8e931bbaf53fc9dc1b2
Christian Miljkovic <chr.miljkovic@live.com>
2020-12-25 20:55:27 +0100

    integrated firebase into app, created sign in page with sign in component and sign in with google and created the sign up part

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBtkHPU1Kxm1XJuITtdGq6JgIJU8QFGPmg",
+	  apiKey: "AIzaSyBtkHPU1Kxm1XJuITtdGq6JgIJU8QFGPmg",
     authDomain: "crwn-db-6d5c0.firebaseapp.com",
     projectId: "crwn-db-6d5c0",
     storageBucket: "crwn-db-6d5c0.appspot.com",
@@ -13,6 +13,31 @@ const config = {
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
