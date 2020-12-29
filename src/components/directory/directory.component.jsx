c0bcb274c0608dcdceec87157180aba009dacef2
Christian Miljkovic <chr.miljkovic@live.com>
2020-12-29 14:51:58 +0100

    moved directory and shop data into its own reducer, created respective selectors and updated corresponding components wiht new redux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
@@ -12,7 +12,7 @@ const Directory = ({ sections }) => (
   <div className='directory-menu'>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
-     ))}
+    ))}
   </div>
 );
 
